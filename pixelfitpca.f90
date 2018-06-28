fe68cfd252b42d2776bb1fd1388ee1ac84c88115
Jason Rowe <Jason.Rowe@ubishops.ca>
2018-06-28 13:32:33 -0400

    Adding GP predictor

--- pixelfitpca.f90
+++ pixelfitpca.f90
@@ -1,10 +1,11 @@
 program pixelfitpca
 use precision
 implicit none
-integer :: iargc,nmax,npt,ixo,iyo
+integer :: iargc,nmax,npt,ixo,iyo,npars,info,nrhs
 real(double) :: minx,mean
 real(double), allocatable, dimension(:) :: x,y,yerr,xpos,ypos,xnep,ynep,&
- oflux,pmod,smod,ax,ay,xpcor,ypcor
+ oflux,pmod,smod,ax,ay,xpcor,ypcor,pars,alpha,yerr2,mu,std,res
+real(double), allocatable, dimension(:,:) :: Kernel,KernelZ
 character(80) :: filename
 
 !These are F90 interfaces that allow one to pass assumed sized arrays
@@ -26,6 +27,13 @@ interface !reads in a three-column ascii space seperated file
       integer :: npt,ixo,iyo
       real(double), dimension(:) :: x,xnep,ynep,ax,ay
    end subroutine fitneptunepos
+   subroutine makekernel(Kernel,npt1,npt2,x1,x2,npt,yerr,npars,pars)
+      use precision
+      implicit none
+      integer, intent(inout) :: npt1,npt2,npars,npt
+      real(double), dimension(:), intent(inout) :: x1,x2,yerr,pars
+      real(double), dimension(:,:), intent(inout) :: Kernel
+   end subroutine makekernel
 end interface
 
 !check that we have enough information from the commandline
@@ -49,7 +57,7 @@ call pgopen('/xserve')  !'?' lets the user choose the device.
 call PGPAP (8.0 ,1.0) !use a square 8" across
 call pgpage() !create a fresh page
 call pgslw(3) !thicker lines
-call pgask(.false.)
+call pgask(.true.)
 
 !need fits to the motion of Neptune - raw data is too noisy.
 !also means that pixel-crossings is now a linear function (good!)
@@ -61,6 +69,63 @@ xpcor=xnep+xpos !correct for pointing jitter
 ypcor=ynep+ypos
 call fitNeptunePos(npt,x,xpcor,ypcor,ixo,ax,iyo,ay)
 deallocate(xpcor,ypcor)
+
+!Here are the parameters that control the co-variance matrix and fitted
+!parameters
+npars=4 !number of parameters used for model of the matrix
+allocate(pars(npars))  !model parameters for Kernel generation.
+pars(1)=1.0d0 !amp scale for exp
+pars(2)=0.72d0 !length scale for exp
+pars(3)=1.0 !second amp scale
+pars(4)=2.0 !second length scale
+
+!lets make a Kernel/co-variance for the Gaussian process
+allocate(Kernel(npt,npt)) !allocate space
+call makekernel(Kernel,npt,npt,x,x,npt,yerr,npars,pars) !create Kernel
+
+!Cholesky factorization
+write(0,*) "Beginning Cholesky factorization"
+call dpotrf('U',npt,Kernel,npt,info) !LAPACK routine for Cholesky
+if (info.ne.0) then !check for errors
+   write(0,*) "Cholesky factorization failed"
+   write(0,*) "dpotrf info: ",info
+   stop
+endif
+
+write(0,*) "Beginning Cholesky Solver"
+!calculate solution
+! We have to solve,
+! Kernel*X=Y,
+! for X.
+! LAPACK routine dpotrs uses Kfac from dpotrf.  We copy y into a new
+! array called alpha, which is overridden with the solution on completion.
+allocate(alpha(npt))
+alpha=y(1:npt) !dpotrs takes alpha as input and output.
+nrhs=1 !how man columns does alpha contain - just one
+call dpotrs('U',npt,nrhs,Kernel,npt,alpha,npt,info) !call LAPACK
+if (info.ne.0) then !check for errors
+   write(0,*) "Solver failed.."
+   write(0,*) "dpotrs info: ",info
+   stop
+endif
+
+!predict samples
+allocate(KernelZ(npt,npt))
+allocate(yerr2(npt),mu(npt),std(npt))
+yerr2=0.0d0
+call makekernel(KernelZ,npt,npt,x,x,npt,yerr2,npars,pars)
+allocate(res(npt))
+
+!$OMP PARALLEL
+!$OMP WORKSHARE
+mu=matmul(KernelZ,alpha)
+std=0.0d0
+res(1:npt)=y(1:npt)-mu(1:npt)
+!$OMP END WORKSHARE
+!$OMP END PARALLEL
+
+write(0,*) "done plotting"
+call plotsamples(npt,x,mu,std) !plot our predicted sample set on top.
 
 call pgclos()
 
